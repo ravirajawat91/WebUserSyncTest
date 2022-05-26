8de7df1c26a2f8d55ff42673b711baf5c6554fbd
Ravindra Singh <ravindrasingh@Ravindras-MacBook-Air.local>
2022-05-26 01:04:30 +0100

    firsr

---
+++ force-app/main/default/aura/MissingWebUserList/MissingWebUserListController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jesus.cantero on 13/05/2021.
+ */
+
+({
+    init: function (cmp, event, helper) {
+        cmp.set('v.columns', [
+            {label: 'Id', fieldName: 'id', type: 'text'},
+            {label: 'Name', fieldName: 'name', type: 'text'},
+            {label: 'UserName', fieldName: 'userName', type: 'text'},
+            {label: 'Email', fieldName: 'email', type: 'email'},
+            {label: 'Company Name', fieldName: 'company', type: 'text'}
+        ]);
+    }
+});
---
+++ force-app/main/default/triggers/WebUserTrigger.trigger
@@ -0,0 +1,7 @@
+/**
+ * Created by jesus.cantero on 13/05/2021.
+ */
+
+trigger WebUserTrigger on Web_User__c (before insert, before update, before delete, after insert, after update, after delete, after undelete) {
+
+}
